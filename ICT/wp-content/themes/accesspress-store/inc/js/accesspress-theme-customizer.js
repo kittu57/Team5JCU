bedef96ed394420ef8be919d8f29382b4cff4f36
kittu57 <kittuduggina@gmailcom>
2015-10-02 11:48:09 +1000

    Update 1 by Kittu
    the whole project

---
+++ ICT/wp-content/themes/accesspress-store/inc/js/accesspress-theme-customizer.js
@@ -0,0 +1,9 @@
+( function( $ ) {
+	//Update site title color in real time...
+//	wp.customize( 'mytheme_options[link_textcolor]', function( value ) {
+//		value.bind( function( newval ) {
+//			$('a').css('color', newval );
+//		} );
+//	} );
+	
+} )( jQuery );
---
+++ ICT/wp-content/themes/accesspress-store/woocommerce/global/sidebar.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Sidebar
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     1.6.4
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+?>
+<!-- #secondary -->
+<?php
+dynamic_sidebar( 'shop' );
+?>
