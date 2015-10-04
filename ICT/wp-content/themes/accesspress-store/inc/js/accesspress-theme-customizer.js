a193ce830bf272025fc301ac34fab09ea93b5531
khushi99 <khushmeet.d@gmail.com>
2015-10-04 16:27:38 +1000

    k

--- ICT/wp-content/themes/accesspress-store/inc/js/accesspress-theme-customizer.js
+++
@@ -1,9 +0,0 @@
-( function( $ ) {
-	//Update site title color in real time...
-//	wp.customize( 'mytheme_options[link_textcolor]', function( value ) {
-//		value.bind( function( newval ) {
-//			$('a').css('color', newval );
-//		} );
-//	} );
-	
-} )( jQuery );
